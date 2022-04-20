f047a03aa1a8fe1e7bfab682d3dc4a4e0c904b9a
Alex <a.caignard@hotmail.fr>
2022-04-20 14:27:39 +0200

    Cours 14 et 15

--- livres.php
+++ livres.php
@@ -1,11 +1,18 @@
 <?php
 require_once "Livre.class.php";
+require_once "LivreManager.class.php";
+
 $l1 = new Livre(1, "Algorithmique selon H2PROG", 300, "algo.png");
 $l2 = new Livre(2, "Le virus Asiatique", 200, "virus.png");
 $l3 = new Livre(3, "La France du 19ème", 100, "france.png");
 $l4 = new Livre(4, "Le JavaScript Client", 500, "JS.png");
 
-$livres = [$l1, $l2, $l3, $l4];
+$livreManager = new LivreManager;
+$livreManager->ajoutLivre($l1);
+$livreManager->ajoutLivre($l2);
+$livreManager->ajoutLivre($l3);
+$livreManager->ajoutLivre($l4);
+
 ob_start()
 ?>
 
@@ -16,7 +23,10 @@ ob_start()
         <th>Nombre de pages</th>
         <th colspan="2">Actions</th>
     </tr>
-    <?php for ($i = 0; $i < count($livres); $i++) : ?>
+    <?php
+    $livres = $livreManager->getLivres();
+    for ($i = 0; $i < count($livres); $i++) :
+    ?>
     <tr>
         <td class="align-middle"><img src="public/images/<?= $livres[$i]->getImage(); ?>" width="60px;"></td>
         <td class="align-middle"><?= $livres[$i]->getTitre(); ?></td>
